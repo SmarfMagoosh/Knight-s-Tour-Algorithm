fbd636e3a30626331b17ecd4999d49d2a6bc8b4b
SmarfMagoosh <82959580+SmarfMagoosh@users.noreply.github.com>
2022-11-05 18:53:44 -0400

    update

--- src/algorithms/Main.java
+++ src/algorithms/Main.java
@@ -1,28 +1,11 @@
 package algorithms;
 
+import java.io.FileNotFoundException;
+
 public class Main {
-	public static void main(String[] args) {
-		Tour t1 = new Tour(5);
-		Tour t2 = new Tour(5, 6);
-		Tour t3 = new Tour(6);
-		Tour t4 = new Tour(6, 5);
-		t1.start();
-		t2.start();
-		t3.start();
-		t4.start();
-		Tour big = Tour.joinTours(t1, t2, t3, t4);
-		System.out.println(big);
-	}
-	
-	public static int testBoard(int length, int width, int tests) {
-		int total = 0;
-		for(int i = 0; i < tests; i++) {
-			Tour kt = new Tour(length, width);
-			kt.start();
-			System.out.println(kt);
-			total += kt.runtime();
-			System.out.println("Test " + (i + 1) + " complete.");
-		}
-		return total / tests;
+	public static void main(String[] args) throws FileNotFoundException {
+		Tour kt = new Tour(10, 8);
+		kt.start();
+		System.out.println(kt);
 	}
 }
